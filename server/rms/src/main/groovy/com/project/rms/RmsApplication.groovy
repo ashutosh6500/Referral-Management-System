b37cfa15dbb652383442e760623a2c1fb82e8712
Ashutosh <ashutoshdkodavade@gmail.com>
2023-06-03 10:44:51 +0530

    spring initializer

---
+++ server/rms/src/main/groovy/com/project/rms/RmsApplication.groovy
@@ -0,0 +1,13 @@
+package com.project.rms
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RmsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RmsApplication, args)
+	}
+
+}
